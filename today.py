f5f8d79c7df09372fc3e4208095749599e204b6b
Gathan Ega Fyandhika <gathanegafyandika@gmail.com>
2026-09-17 15:05:39 +0700

    Update today.py

--- today.py
+++ today.py
@@ -6,9 +6,9 @@ from lxml import etree
 import time
 import hashlib
 
-HEADERS = {'authorization': 'token ' + os.environ['ACCESS_TOKEN']}  
-USER_NAME = os.environ['USER_NAME']                                  
-BIRTHDAY_ENV = os.environ.get('BIRTHDAY')                      
+HEADERS = {'authorization': 'token ' + os.environ['ACCESS_TOKEN']}
+USER_NAME = os.environ['USER_NAME']
+BIRTHDAY_ENV = os.environ.get('BIRTHDAY')  # contoh: '2003-04-12'
 
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 
